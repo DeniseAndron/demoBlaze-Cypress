55c0284d37f1e4d0d577470c658bc2347c09efe0
denisa <denise.andron@protonmail.com>
2023-09-05 12:08:00 +0300

    added reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results/mochawesome',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+  reporterEnabled: 'spec, mocha-junit-reporter',
+  mochaJunitReporterReporterOptions: {
+    mochaFile: 'cypress/results/results-[hash].xml',
+  },
   viewportHeightL: 1080,
   viewportWidth: 1920,
   video:false,
@@ -9,8 +24,14 @@ module.exports = defineConfig({
     password: 'Welcome123',
     apiURL: 'https://api.demoblaze.com'
   },
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
   e2e: {
     
       specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
-  }
+  },
+  
+  
 })
